1c087c752858dc65a446354f9804d3a23ff78fa6
Civerra Lucio <lciverra@gmail.com>
2025-06-05 10:25:00 +0200

    Update copy_sp_to_server.bat

--- copy_sp_to_server.bat
+++ copy_sp_to_server.bat
@@ -1,4 +1,1 @@
-pscp -pw Lagora_15 C:\suipassi\ultimo\dist\sp.war root@5.249.150.203:/opt/tomcat9/
 
-plink -pw Lagora_15 root@5.249.150.203 "mv /opt/tomcat9/sp.war /opt/tomcat9/webapps/sp.war"
-pauseECHO FINE
